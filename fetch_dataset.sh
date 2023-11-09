b5839204ead224f14932c6919a5da7feafc26ad7
Xinyu Fang <f813779259@gmail.com>
2023-11-09 10:42:52 -0500

    push

--- fetch_dataset.sh
+++ fetch_dataset.sh
@@ -5,16 +5,16 @@ mkdir -p dataset/argoverse
 cd dataset/argoverse
 
 # Download
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_val_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_test_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/hd_maps.tar.gz
+#wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
+#wget https://s3.amazonaws.com/argoai-argoverse/forecasting_val_v1.1.tar.gz
+#wget https://s3.amazonaws.com/argoai-argoverse/forecasting_test_v1.1.tar.gz
+#wget https://s3.amazonaws.com/argoai-argoverse/hd_maps.tar.gz
 
 # Extract
-tar xvf forecasting_train_v1.1.tar.gz
-tar xvf forecasting_val_v1.1.tar.gz
-tar xvf forecasting_test_v1.1.tar.gz
-tar xf hd_maps.tar.gz
+#tar xvf forecasting_train_v1.1.tar.gz
+#tar xvf forecasting_val_v1.1.tar.gz
+#tar xvf forecasting_test_v1.1.tar.gz
+#tar xf hd_maps.tar.gz
 
 # Argoverse needs the map_files folder
 SITE_PACKAGES=$(python -c 'import site; print(site.getsitepackages()[0])')
